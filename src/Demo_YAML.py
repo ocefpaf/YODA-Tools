b37b09f74ccee00c14f263cc4d3c293ba27330fc
Stephanie Reeder <stephanie.reeder@usu.edu>
2015-09-21 17:45:12 -0600

    update python imports to api

--- src/Demo_YAML.py
+++ src/Demo_YAML.py
@@ -5,36 +5,38 @@ import pprint
 
 pp = pprint.PrettyPrinter(indent=8)
 
-this_file = os.path.realpath(__file__)
-print("This file: ", this_file)
-directory = os.path.dirname(this_file)
-print("Directory: ", directory, os.listdir(directory))
-api_directory = os.path.join(directory, 'ODM2PythonAPI', 'src', 'api')
-src_directory = os.path.join(directory, 'src')
+# this_file = os.path.realpath(__file__)
+# print("This file: ", this_file)
+# directory = os.path.dirname(this_file)
+# print("Directory: ", directory, os.listdir(directory))
+# #api_directory = os.path.join(directory, 'ODM2PythonAPI', 'YAML', 'api')
+# src_directory = os.path.join(directory, 'YAML')
+#
+# #print("API_PATH: ", api_directory)
+# print("SRC_PATH: ", src_directory)
+#
+#
+# #if not api_directory in sys.path:
+# #    sys.path.append(api_directory)
+# if not src_directory in sys.path:
+#     sys.path.append(src_directory)
+#
+# # pp.pprint(sys.path)
+#
+# try:
+#     # check to make sure that these imports happen
+#     from src.api.ODM2.models import *
+#     # from ODM2PythonAPI.src.api.ODM2.new_services import createService
+#     from src.api.ODMconnection import dbconnection
+#     from YAML.yamlFunctions import YamlFunctions
+# except ImportError as e:
+#     print(e)
+#     sys.exit(0)
 
-print("API_PATH: ", api_directory)
-print("SRC_PATH: ", src_directory)
 
-
-if not api_directory in sys.path:
-    sys.path.append(api_directory)
-if not src_directory in sys.path:
-    sys.path.append(src_directory)
-
-# pp.pprint(sys.path)
-
-try:
-    # check to make sure that these imports happen
-    from src.api.ODM2.models import *
-    # from ODM2PythonAPI.src.api.ODM2.new_services import createService
-    from src.api.ODMconnection import dbconnection
-    from YAML.yamlFunctions import YamlFunctions
-except ImportError as e:
-    print(e)
-    sys.exit(0)
-
-
-
+from api.ODM2.models import *
+from api.ODMconnection import dbconnection
+from YAML.yamlFunctions import YamlFunctions
 
 # Create a connection to the ODM2 database
 
@@ -43,8 +45,9 @@ except ImportError as e:
 
 # session_factory = dbconnection.createConnection('mssql', '(local)', 'ODM2SS', 'ODM', 'odm')
 # conn = dbconnection.createConnection('postgresql', 'localhost', 'ODMSS', 'Stephanie', 'odm')
-session_factory = dbconnection.createConnection('mysql', "localhost", 'ODM2', 'ODM', 'odm')
+# session_factory = dbconnection.createConnection('mysql', "localhost", 'ODM2', 'ODM', 'odm')
 
+session_factory = dbconnection.createConnection('mysql', 'jws.uwrl.usu.edu', 'ODM2', 'ODM', 'ODM123!!')
 # Create a connection for each of the schemas. Currently the schemas each have a different
 # connection but it will be changed to all the services sharing a connection
 # ----------------------------------------------------------------------------------------
@@ -74,6 +77,7 @@ files = []
 # files.append(os.path.join('.', 'YODA-File', 'ExcelTemplates', 'Prototypes', 'Timeseries_Template_Working.yaml'))
 # files.append(os.path.join('.', 'YODA-File', 'ExcelTemplates', 'Prototypes', 'Timeseries_Template_Working_modified.yaml'))
 files.append(os.path.join('.', 'test.yaml'))
+# files.append('/Users/stephanie/Documents/YODA_Specimen_TEMPLATE_NOERRORS.xlsm')
 ## Working files
 # files.append(os.path.join('.', 'ODM2/YAML/Examples/iUTAH_MultiTimeSeriesExample_CompactHeader.yaml'))
 
@@ -102,7 +106,7 @@ sampling_features = _session.query(SamplingFeatures).limit(50).all()
 sites = _session.query(Sites).limit(50).all()
 
 methods = _session.query(Methods).limit(50).all()
-variables = _session.query(Variables).alimit(50).ll()
+variables = _session.query(Variables).limit(50).all()
 units = _session.query(Units).limit(50).all()
 processing_levels = _session.query(ProcessingLevels).limit(50).all()
 actions = _session.query(Actions).limit(50).all()
