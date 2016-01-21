9aad9244dd6a41c5a59b09f7d3475af092171156
cyoun <cdesyoun@gmail.com>
2016-01-21 15:06:47 -0800

    moved "test" location

---
+++ src/yoda_tools/tests/test_validation.py
@@ -0,0 +1,20 @@
+__author__ = 'cyoun'
+import unittest
+import sys
+import os.path
+
+curr_folder = os.path.abspath(os.path.dirname(__file__))
+sys.path.insert(0, '%s/../../../' % curr_folder)
+print curr_folder
+
+from src.yoda_tools.yoda import validate_timeseries
+
+class validateYodaTestCases(unittest.TestCase):
+    def test_goodFile(self, file='../examples/YODA_TimeSeries_Example1_Template_0.3.0-alpha_fixed.yaml'):
+        flag =  validate_timeseries(file)
+        self.assertTrue(flag, 'Timeseries_Template_Working_my_version.yaml failed')
+
+    def test_badFile(self, file='../examples/YODA_TimeSeries_Example1_Template_0.3.0-alpha.yaml'):
+        flag =  validate_timeseries(file)
+        self.assertFalse(flag,'Timeseries_Template_Working.yaml failed to find errors' )
+
