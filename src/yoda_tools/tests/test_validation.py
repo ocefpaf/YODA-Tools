7f26297f5fe5ad4faab80453eb6d81ef051a4720
cyoun <cdesyoun@gmail.com>
2016-01-21 15:09:17 -0800

    update text

--- src/yoda_tools/tests/test_validation.py
+++ src/yoda_tools/tests/test_validation.py
@@ -12,9 +12,9 @@ from src.yoda_tools.yoda import validate_timeseries
 class validateYodaTestCases(unittest.TestCase):
     def test_goodFile(self, file='../examples/YODA_TimeSeries_Example1_Template_0.3.0-alpha_fixed.yaml'):
         flag =  validate_timeseries(file)
-        self.assertTrue(flag, 'Timeseries_Template_Working_my_version.yaml failed')
+        self.assertTrue(flag, 'YODA_TimeSeries_Example1_Template_0.3.0-alpha_fixed.yaml failed')
 
     def test_badFile(self, file='../examples/YODA_TimeSeries_Example1_Template_0.3.0-alpha.yaml'):
         flag =  validate_timeseries(file)
-        self.assertFalse(flag,'Timeseries_Template_Working.yaml failed to find errors' )
+        self.assertFalse(flag,'YODA_TimeSeries_Example1_Template_0.3.0-alpha.yaml failed to find errors' )
 
