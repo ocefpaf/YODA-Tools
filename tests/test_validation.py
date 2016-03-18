9c7b05e2fbec81ce8b9105c0075f887ba169ba06
cyoun <cdesyoun@gmail.com>
2016-03-18 14:01:41 -0700

    add unit tests

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -1,20 +1,43 @@
 __author__ = 'cyoun'
 import os.path
 import sys
+import logging
 import unittest
+import yaml
 
-curr_folder = os.path.abspath(os.path.dirname(__file__))
-sys.path.insert(0, '%s/../../' % curr_folder)
-print curr_folder
+# curr_folder = os.path.abspath(os.path.dirname(__file__))
+# sys.path.insert(0, '%s/../' % curr_folder)
+# print curr_folder
 
-from yodatool import validate_timeseries
+logger = logging.getLogger("Test.YODATool")
+
+from yodatool.validate.timeseriesvalidator import TSvalidator
+from yodatool.validate.cvvalidator import CVvalidator
 
 class validateYodaTestCases(unittest.TestCase):
-    def test_goodFile(self, file='../examples/YODA_TimeSeries_Example1_Template_0.3.1-alpha.yaml'):
-        flag =  validate_timeseries(file)
-        self.assertTrue(flag, 'YODA_TimeSeries_Example1_Template_0.3.1-alpha.yaml failed')
 
-    def test_badFile(self, file='../examples/YODA_TimeSeries_Example1_Template_0.3.0-alpha.yaml'):
-        flag =  validate_timeseries(file)
-        self.assertFalse(flag,'YODA_TimeSeries_Example1_Template_0.3.0-alpha.yaml failed to find errors' )
+    def setUp(self):
+        yodafile='../yodatool/examples/YODA_TimeSeries_Example1_Template_0.3.1-alpha.yaml'
+        stream = file(yodafile)
+        yaml_data = yaml.load(stream)
 
+        if 'YODA' in yaml_data:
+            yaml_data.pop('YODA')
+
+        self.data = yaml_data
+
+    def test_dataTypes(self):
+
+        tsvalidator = TSvalidator(logger)
+        flag = tsvalidator.validate(3,self.data)
+
+        self.assertTrue(flag, 'Data type validation is OK.')
+
+    def test_controlledVocaburary(self):
+        cvvalidator = CVvalidator(logger)
+        flag = cvvalidator.validate(self.data)
+
+        self.assertTrue(flag,'CV validation is OK.' )
+
+if __name__ == '__main__':
+    unittest.main()
