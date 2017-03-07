5a4ebe366d623e03f73e52bac7df2afd36d6ae0a
sreeder <stephanie.reeder@usu.edu>
2017-03-07 16:45:38 -0700

    merge with amster

--- DataLoader/Model/Inputs/yamlInput.py
+++ DataLoader/Model/Inputs/yamlInput.py
@@ -4,3 +4,11 @@ class yamlInput(iInputs):
     def __init__(self):
         pass
 
+    def parse(self):
+        pass
+
+    def verify(self):
+        pass
+
+    def sendODM2Session(self):
+        pass
