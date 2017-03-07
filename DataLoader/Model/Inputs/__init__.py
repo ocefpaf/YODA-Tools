a0be38bc840e307db2b7de34fbfe18444b81e147
Mikaila Young <young.mikaila@gmail.com>
2017-03-07 15:13:26 -0700

    input skeleton flesh

---
+++ DataLoader/Model/Inputs/__init__.py
@@ -0,0 +1,5 @@
+from DataLoader.Model.Abstract.iInputs import iInputs
+from DataLoader.Model.Inputs.excelInput import excelInput
+from DataLoader.Model.Inputs.yamlInput import yamlInput
+
+__all__ = ['iInputs', 'excelInput', 'yamlInput']
---
+++ DataLoader/Model/Inputs/excelInput.py
@@ -0,0 +1,13 @@
+from DataLoader.Model.Abstract import iInputs
+
+
+class excelInput(iInputs):
+
+    def parse(self):
+        pass
+
+    def verify(self):
+        pass
+
+    def sendODM2Session(self):
+        pass
