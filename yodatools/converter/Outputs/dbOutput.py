6c4017a8436e3dded3f730706f131ae1bd485aa9
Mikaila Young <young.mikaila@gmail.com>
2017-04-26 18:22:57 -0600

    DB output

--- yodatools/converter/Outputs/dbOutput.py
+++ yodatools/converter/Outputs/dbOutput.py
@@ -6,14 +6,27 @@ import sqlite3
 
 class dbOutput(iOutputs):
 
-    def save(self, session, file_path):
+    def __init__(self, file_path, connection_string = None):
+        if connection_string:
+            self.connect_to_db(connection_string)
+
+    def connect_to_db(self, connection_string):
+        self.session_factory_out = dbconnection.createConnectionFromString(connection_string)
+        self._session_out = self.session_factory_out.getSession()
+        self._engine = self.session_factory_out.engine
+        return self._session_out
+
+    def save(self, session, file_path, connection_string):
         tables = self.parseObjects()
-        data = []
+        session_out = self.create_db_connection()
+        #data = []
+
         for t in tables:
             try:
                 for o in session.query(t).all():
-                    data.append(o)
-                    #### WRITE TO DB
+                    #data.append(o)
+                    session_out.add(o)
+
             except Exception as e:
                 print e
 
