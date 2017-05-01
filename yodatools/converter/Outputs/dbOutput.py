70f6348a97ca5a9bdf61c22069c8a90e89020cd2
sreeder <stephanie.reeder@usu.edu>
2017-05-01 17:51:56 -0600

    trying to figure out why now site returned

--- yodatools/converter/Outputs/dbOutput.py
+++ yodatools/converter/Outputs/dbOutput.py
@@ -1,7 +1,7 @@
 from yodatools.converter.Abstract import iOutputs
 from odm2api.ODMconnection import dbconnection
 from odm2api.ODM2.services import *
-from odm2api.ODM2.models import setSchema
+from odm2api.ODM2.models import setSchema, Sites, Results, SamplingFeatures, Specimens, MeasurementResults, TimeSeriesResults
 import logging
 import sqlite3
 
@@ -21,140 +21,148 @@ class dbOutput(iOutputs):
 
     def save(self, session, connection_string):
         self.data = self.parseObjects(session)
-        session_out = self.connect_to_db(connection_string)
+        self.connect_to_db(connection_string)
         
         #datasets
-        self.check("datasets", self.data, session_out)
+        self.check("datasets", self.data)
         # organization
-        self.check("organizations", self.data, session_out)
+        self.check("organizations", self.data)
         # people
-        self.check("people", self.data, session_out)
+        self.check("people", self.data)
         # affiliations
-        self.check("affiliations", self.data, session_out)
+        self.check("affiliations", self.data)
         # citations
-        self.check("citations", self.data, session_out)
+        self.check("citations", self.data)
         # authorlists
-        self.check("authorlists", self.data, session_out)
+        self.check("authorlists", self.data)
         # datasetcitations
-        self.check("datasetcitations", self.data, session_out)
+        self.check("datasetcitations", self.data)
         # spatialreferences
-        self.check("spatialreferences", self.data, session_out)
+        self.check("spatialreferences", self.data)
         # samplingfeatures: 
-        self.check("samplingfeatures", self.data, session_out)
+        self.check("samplingfeatures", self.data)
         # relatedfeatures
-        self.check("relatedfeatures", self.data, session_out)
+        self.check("relatedfeatures", self.data)
         # units
-        self.check("units", self.data, session_out)
+        self.check("units", self.data)
         # annotations
-        self.check("annotations", self.data, session_out)
+        self.check("annotations", self.data)
         # methods
-        self.check("methods", self.data, session_out)
+        self.check("methods", self.data)
         # variables
-        self.check("variables", self.data, session_out)
+        self.check("variables", self.data)
         # proc level
-        self.check("processinglevels", self.data, session_out)
+        self.check("processinglevels", self.data)
         # action
-        self.check("actions", self.data, session_out)
+        self.check("actions", self.data)
         # featureaction
-        self.check("featureactions", self.data, session_out)
+        self.check("featureactions", self.data)
         # actionby
-        self.check("actionby", self.data, session_out)
+        self.check("actionby", self.data)
         # relatedActions
-        self.check("relatedactions", self.data, session_out)
-        # result 
-        self.check("results", self.data, session_out)
+        self.check("relatedactions", self.data)
+        # result
+        self.check("results", self.data)
         # datasetresults
-        self.check("datasetsresults", self.data, session_out)
+        self.check("datasetsresults", self.data)
         # measurementResultValues
-        self.check("measurementresultvalues", self.data, session_out)
+        self.check("measurementresultvalues", self.data)
         # MeasurementResultValueAnnotations
-        self.check("measurementresultvalueannotations", self.data, session_out)
+        self.check("measurementresultvalueannotations", self.data)
         # timeseriesresultvalues - ColumnDefinitions:, data:
         val = "timeseriesresultvalues"
         if val in self.data:
-            self.save_ts(self.data[val], session_out)
+            self.save_ts(self.data[val])
         
-        session_out.commit()
+        self.session_out.commit()
 
-    def save_ts(self, values, session_out,):
+    def save_ts(self, values):
         pass
 
-    def check(self, objname, data, session_out):
+    def check(self, objname, data):
 
         if objname in data:
-            vals = self.add_to_db(session_out,  data[objname])
+            vals = self.add_to_db( data[objname])
         self.added_objs[objname] =vals
 
-    def add_to_db(self, session_out,  values):
+    def add_to_db(self,  values):
         added = []
         from odm2api.ODM2.models import _changeSchema
         _changeSchema(None)
         for obj in values:
             try:
                 valuedict = obj.__dict__.copy()
-
-
-                # for v in valuedict:
-                #     if v.lower() == obj.__mapper__.primary_key[0].name:
-                #         primarykey = v
-                #         break
-                # # pop primary key
-                # valuedict.pop(primarykey)
-
-
-                for key in dir(obj):
-                    if "obj" in key.lower():  # key.contains("Obj"):
-                        try:
-
-                            att = getattr(obj, key)
-
-                            if att is not None:
-                                attdict= att.__dict__.copy()
-                                for v in attdict.keys():
-                                #     if v.lower() == obj.__mapper__.primary_key[0].name:
-                                #         del attdict[v]
-                                #         break
-                                    if "id" in v.lower() and "uuid" not in v.lower():
-                                        del attdict[v]
-                                attdict.pop("_sa_instance_state")
-                                setSchema(self._engine_out)
-                                new_obj = session_out.query(type(att)).filter_by(**attdict).first()
-                                objkey = key.replace("Obj", "ID")
-                                newkey = objkey
-                                if objkey =="RelatedFeatureID":
-                                    newkey = "SamplingFeatureID"
-                                elif objkey == "RelatedActionID":
-                                    newkey = "ActionID"
-
-                                valuedict[objkey] = getattr(new_obj, newkey)
-
-                        except Exception as e:
-                            print ("cannot find {} in {}. Error:{} in dbOutput".format(key, obj.__class__.__name__, e))
-
+                valuedict = self.get_new_objects(obj, valuedict)
                 valuedict.pop("_sa_instance_state")
 
                 #delete primary key
-                for v in valuedict:
+                for v in valuedict.keys():
                     if v.lower() == obj.__mapper__.primary_key[0].name:
                         del valuedict[v]
-                        break
-                # for k in valuedict.keys():
-                #     if "id" in k.lower() and "uuid" not in k.lower():
-                #         objkey=k.replace("ID", "Obj")
-                #         valuedict[k] = getattr(obj, objkey)
-                #         # del valuedict[k]
-                added.append(self.get_or_create(session_out, type(obj), **valuedict))
-                # session_out.add(obj)
+                    elif "obj" in v.lower():
+                        del valuedict[v]
+                setSchema(self._engine_out)
+                model = self.check_model(attr=obj)
+                added.append(self.get_or_create(self._session_out, model, **valuedict))
 
             except Exception as e:
                 print e
-                session_out.rollback()
+                self._session_out.rollback()
                 # raise e
         return added
 
+    def check_model(self, attr):
+        model = type(attr)
+        # if isinstance(attr, Sites) or isinstance(attr, Specimens):
+        #     model = SamplingFeatures
+        # elif isinstance(attr, MeasurementResults) or isinstance(attr, TimeSeriesResults):
+        #     return Results
+
+        return model
+
+    def get_new_objects(self, obj, valuedict):
+        for key in dir(obj):
+            if "obj" in key.lower():  # key.contains("Obj"):
+                try:
+                    att = getattr(obj, key)
+                    if att is not None:
+                        attdict = att.__dict__.copy()
+                        # for v in attdict.keys():
+                        #     if "id" in v.lower() and "uuid" not in v.lower():
+                        #         del attdict[v]
+                        for k in attdict.keys():
+                            if k.lower() == att.__mapper__.primary_key[0].name:
+                                del attdict[k]
+                            elif "obj" in k.lower():
+                                del attdict[k]
+                        attdict.pop("_sa_instance_state")
+                        attdict = self.get_new_objects(att, attdict)
+
+                        model = self.check_model(attr =att)
+                        setSchema(self._engine_out)
+
+                        new_obj = self._session_out.query(model).filter_by(**attdict).first()
+                        # for s in self.added_objs["samplingfeatures"]:
+                        #     if s == att:
+                        #         new_obj = s
+
+                        objkey = key.replace("Obj", "ID")
+                        newkey = objkey
+                        if objkey == "RelatedFeatureID":
+                            newkey = "SamplingFeatureID"
+                        elif objkey == "RelatedActionID":
+                            newkey = "ActionID"
+
+
+
+                        valuedict[objkey] = getattr(new_obj, newkey)
+
+                except Exception as e:
+                    print ("cannot find {} in {}. Error:{} in dbOutput".format(key, obj.__class__.__name__, e))
+        return valuedict
+
 
     def get_or_create(self, sess, model, **kwargs):
-        setSchema(self._engine_out)
         instance = sess.query(model).filter_by(**kwargs).first()
         if instance:
             return instance
