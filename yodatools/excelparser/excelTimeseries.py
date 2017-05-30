339d189be7f7e4d8f1811e02aeed48a1c35df095
sreeder <stephanie.reeder@usu.edu>
2017-05-30 17:26:19 -0600

    rename UTCOffset to ValuedatetimeUTCOFfset

--- yodatools/excelparser/excelTimeseries.py
+++ yodatools/excelparser/excelTimeseries.py
@@ -530,6 +530,7 @@ class ExcelTimeseries():
             .reset_index() \
             .rename(columns={0: 'DataValue'}) \
             .rename(columns={'LocalDateTime': 'ValueDateTime'}) \
+            .rename(columns={'LocalDateTime': 'ValueDateTimeUTCOffset'}) \
             .dropna()
 
         for k, v in meta_dict.iteritems():
