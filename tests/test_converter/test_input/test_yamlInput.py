39b378e15564546a1d8e2eb386b9081eb80745e3
sreeder <stephanie.reeder@usu.edu>
2017-11-09 10:09:59 -0700

    uncomment tests

--- tests/test_converter/test_input/test_yamlInput.py
+++ tests/test_converter/test_input/test_yamlInput.py
@@ -10,31 +10,32 @@ class TestYaml:
 
 
     def test_parse_specimen(self):
-        # D:\DEV\YODA - Tools\tests\test_files\test_ts_specimen_output.yaml
+
         file_path = os.path.join(curr_folder, 'test_files', 'test_specimen_ts.yaml')
-        # file_path = os.path.join(curr_folder, 'test_files', 'test_ts_specimen_output.yaml')
 
         self.yi.parse(file_path)
         session = self.yi.sendODM2Session()
 
-        assert session != None
-        from odm2api.ODM2.models import People, SamplingFeatures
+        assert session is not None
+        from odm2api.ODM2.models import People, SamplingFeatures, MeasurementResultValues
         assert len(session.query(People).all()) > 0
         assert len(session.query(SamplingFeatures).all()) > 0
+        assert session.query(MeasurementResultValues).first() is not None
         session.close()
 
-    # def test_parse_ts(self):
-    #     file_path = os.path.join(curr_folder, 'test_files', 'test_ts.yaml')
-    #     # file_path = os.path.join(curr_folder, 'test_files', 'test_ts_output.yaml')
-    #     self.yi.parse(file_path)
-    #
-    #     session = self.yi.sendODM2Session()
-    #
-    #     assert session != None
-    #     from odm2api.ODM2.models import People, SamplingFeatures
-    #     assert len(session.query(People).all()) > 0
-    #     assert len(session.query(SamplingFeatures).all()) > 0
-    #     session.close()
+    def test_parse_ts(self):
+        file_path = os.path.join(curr_folder, 'test_files', 'test_ts.yaml')
+        # file_path = os.path.join(curr_folder, 'test_files', 'test_ts_output.yaml')
+        self.yi.parse(file_path)
+
+        session = self.yi.sendODM2Session()
+
+        assert session != None
+        from odm2api.ODM2.models import People, SamplingFeatures, TimeSeriesResultValues
+        assert len(session.query(People).all()) > 0
+        assert len(session.query(SamplingFeatures).all()) > 0
+        assert session.query(TimeSeriesResultValues).first() is not None
+        session.close()
 
 
 
--- tests/test_converter/test_output/test_dbOutput.py
+++ tests/test_converter/test_output/test_dbOutput.py
@@ -2,7 +2,8 @@ import os
 
 from tests.test_util import build_ts_session, build_ts_specimen_session
 from yodatools.converter.Outputs.dbOutput import dbOutput
-from odm2api import ODMconnection
+from odm2api.ODMconnection import dbconnection
+from odm2api.ODM2.models import People, SamplingFeatures, MeasurementResultValues, TimeSeriesResultValues
 
 
 curr_folder = os.path.abspath(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
@@ -12,9 +13,11 @@ curr_folder = os.path.abspath(os.path.dirname(os.path.dirname(os.path.dirname(__
 class TestDb:
     def setup(self):
 
-        # self.connection_string = 'mysql+pymysql://ODM:odm@localhost/odm2'
-        self.connection_string = 'sqlite://'
-        self.do = dbOutput(self.connection_string)
+        self.connection_string = 'mysql+pymysql://ODM:odm@localhost/odm2'
+        # self.connection_string = 'sqlite://'
+        self.do = dbOutput()
+        session_factory = dbconnection.createConnectionFromString(self.connection_string)
+        self.session_out = session_factory.getSession()
 
 
 
@@ -22,7 +25,17 @@ class TestDb:
         session = build_ts_specimen_session()
         self.do.save(session, self.connection_string)
 
+        assert len(self.session_out.query(People).all()) > 0
+        assert len(self.session_out.query(SamplingFeatures).all()) > 0
+        assert self.session_out.query(MeasurementResultValues).first() is not None
+        self.session_out.close()
+
 
     def test_create_ts(self):
         session = build_ts_session()
         self.do.save(session, self.connection_string)
+
+        assert len(self.session_out.query(People).all()) > 0
+        assert len(self.session_out.query(SamplingFeatures).all()) > 0
+        assert self.session_out.query(TimeSeriesResultValues).first() is not None
+        self.session_out.close()
